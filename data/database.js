c4d19d210260bf5d60b09b3d067317ad45893c98
Frantisek.Huber <architekturnik@gmail.com>
2020-06-17 16:14:34 +0200

    added user data to git

---
+++ data/database.js
@@ -0,0 +1,100 @@
+{
+    "config": {
+        "siteTitle": "FISMEDICA s.r.o",
+        "theme": "clean",
+        "defaultPage": "home-1",
+        "login": "admin",
+        "loggedIn": false,
+        "forceLogout": false,
+        "password": "$2y$10$8ASsMYuMidIQh2i8ffym0OaTd8c\/KHf8ESJd2cwzfYco.bOojzeiO",
+        "lastLogins": {
+            "2020\/06\/17 09:55:50": "::1",
+            "2020\/06\/17 09:48:26": "::1"
+        },
+        "defaultRepos": {
+            "themes": {
+                "0": "https:\/\/github.com\/robiso\/essence",
+                "1": "https:\/\/github.com\/robiso\/violet",
+                "2": "https:\/\/github.com\/robiso\/universeGradient",
+                "3": "https:\/\/github.com\/robiso\/default",
+                "4": "https:\/\/github.com\/StephanStanisic\/theme-w3css-singlepage",
+                "5": "https:\/\/github.com\/robiso\/material",
+                "6": "https:\/\/github.com\/robiso\/parallax",
+                "7": "https:\/\/github.com\/robiso\/oldDefault",
+                "8": "https:\/\/github.com\/robiso\/abstract",
+                "9": "https:\/\/github.com\/robiso\/bike",
+                "10": "https:\/\/github.com\/robiso\/black",
+                "11": "https:\/\/github.com\/robiso\/clean",
+                "12": "https:\/\/github.com\/robiso\/carbide",
+                "13": "https:\/\/github.com\/robiso\/darkBlue",
+                "14": "https:\/\/github.com\/robiso\/fallout",
+                "15": "https:\/\/github.com\/robiso\/gold",
+                "16": "https:\/\/github.com\/robiso\/green",
+                "17": "https:\/\/github.com\/robiso\/lightBlue",
+                "18": "https:\/\/github.com\/robiso\/paper-l",
+                "19": "https:\/\/github.com\/robiso\/paper-p",
+                "20": "https:\/\/github.com\/robiso\/pink",
+                "21": "https:\/\/github.com\/robiso\/purple",
+                "22": "https:\/\/github.com\/robiso\/red",
+                "23": "https:\/\/github.com\/robiso\/supermario",
+                "24": "https:\/\/github.com\/robiso\/turbosDefault",
+                "25": "https:\/\/github.com\/robiso\/wcms-theme-adivvyo"
+            },
+            "plugins": {
+                "0": "https:\/\/github.com\/robiso\/summernote-editor",
+                "1": "https:\/\/github.com\/robiso\/simple-blog",
+                "2": "https:\/\/github.com\/robiso\/simple-statistics",
+                "3": "https:\/\/github.com\/robiso\/additional-contents",
+                "4": "https:\/\/github.com\/robiso\/hits-counter",
+                "5": "https:\/\/github.com\/robiso\/summernote-air-editor",
+                "6": "https:\/\/github.com\/robiso\/contact-form",
+                "7": "https:\/\/github.com\/robiso\/translation-german",
+                "8": "https:\/\/github.com\/robiso\/translation-russian",
+                "9": "https:\/\/github.com\/robiso\/translation-dutch",
+                "10": "https:\/\/github.com\/robiso\/translation-polish",
+                "11": "https:\/\/github.com\/robiso\/translation-slovenian"
+            },
+            "lastSync": "2020\/06\/17"
+        },
+        "customRepos": {
+            "themes": {},
+            "plugins": {}
+        },
+        "menuItems": {
+            "0": {
+                "name": "Home",
+                "slug": "home-1",
+                "visibility": "hide"
+            }
+        }
+    },
+    "pages": {
+        "404": {
+            "title": "404",
+            "keywords": "404",
+            "description": "404",
+            "content": "<h1>Sorry, page not found. :(<\/h1>"
+        },
+        "home-1": {
+            "title": "Home",
+            "keywords": "Keywords, are, good, for, search, engines",
+            "description": "A short description is also good.",
+            "content": "<h2>Kontakt<\/h2>\n<p class=\"lead\">MuDr. Jitka Skotáková - praktická lékařka pro dospělé<br>\nJana Knotková - zdravotní sestra<\/p>\n\n<p><span class=\"lead\">+420 516 838 405<br>\n+420 734 112 712<\/span> (pouze v ordinačních hodinách, poté je mobil vypnut)<\/p>\n\n<h3>Ordinační hodiny<\/h3>\n<table class=\"table table-sm table-bordered\">\n<thead>\n<tr>\n  <th>-<\/th>\n  <th>akutní případy<\/th>\n  <th>pro objednané<\/th>\n<\/tr>\n<\/thead>\n<tbody>\n<tr>\n  <td>Pondělí<\/td>\n  <td>7:30 - 11:30<\/td>\n  <td>11:30 - 13:00<\/td>\n<\/tr>\n<tr>\n  <td>Úterý<\/td>\n  <td>9:00 - 16:00<\/td>\n  <td>16:00 - 18:00<\/td>\n<\/tr>\n<tr>\n  <td>Středa<\/td>\n  <td>7:30 - 11:30<\/td>\n  <td>11:30 - 13:00<\/td>\n<\/tr>\n<tr>\n  <td>Čtvrtek<\/td>\n  <td>7:30 - 11:30<\/td>\n  <td>11:30 - 13:00<\/td>\n<\/tr>\n<tr>\n  <td>Pátek<\/td>\n  <td>7:30 - 12:00<\/td>\n  <td><\/td>\n<\/tr>\n<\/tbody>\n<\/table>\n<i class=\"d-inline-block mb-4\">Odběry jsou od 7:00 do 8:00<\/i>\n<h4>Ordinace<\/h4>\n<p><a href=\"https:\/\/goo.gl\/maps\/piZPaBCr28j1eF1L8\">Sadová 1596\/33, 678 01 Blansko<\/a> - areál Nemocnic Blansko<br>\n\n<\/p><div class=\"embed-container my-4\"><iframe src=\"https:\/\/www.google.com\/maps\/embed?pb=!1m18!1m12!1m3!1d2598.5025550412106!2d16.650004115692237!3d49.361563079339994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47128e9c2060b0f9%3A0x9dda8f760f35d275!2sSadov%C3%A1%201596%2F33%2C%20678%2001%20Blansko!5e0!3m2!1scs!2scz!4v1592396054206!5m2!1scs!2scz\" width=\"600\" height=\"450\" frameborder=\"0\" style=\"border:0;\" allowfullscreen=\"\" aria-hidden=\"false\" tabindex=\"0\"><\/iframe><\/div>\n\n<h4>Pojišťovny<\/h4>\n\n<ul>\n<li>111 - Všeobecná zdravotní pojišťovna ČR<\/li>\n<li>211 - Zdravotní pojišťovna ministerstva vnitra ČR<\/li>\n<li>205 - Česká průmyslová zdravotní pojišťovna<\/li>\n<li>201 - Vojenská zdravotní pojišťovna ČR<\/li>\n<li>207 - Oborová zdravotní pojišťovna zaměstnanců bank, pojišťoven a stavebnictví<\/li>\n<\/ul>"
+        },
+        "home": null
+    },
+    "blocks": {
+        "subside": {
+            "content": "<h2>Doporučený postup:<\/h2>\n\n<p><b>ODLOŽIT<\/b> kontroly chronických pacientů bez akutních potíží. Odložit též veškeré preventivní prohlídky, očkování atd. Pravidelnou medikaci řešit vystavováním eReceptu.<\/p>\n\n<p>Pacientům v karanténě vystavit e-Neschopenku pouze na základě telefonické konzultace. Pacienty s, ve vztahu k dané situaci !, banálními problémy řešit mailově nebo telefonicky. Stejně tak u všech, kde je to z medicínského hlediska možné ( drobné úrazy, bolesti zad , kožní problémy..)<\/p>\n\n<p><b>REDUKOVAT ORDINAČNÍ HODINY<\/b>, je-li to možné zkrátit pro perzonál dobu expozice virem na minimum<\/p>\n\n<p><b>JE NEPŘÍPUSTNÉ<\/b> z epidemiologických důvodů mísení zdravé populace s nemocnými v čekárnách a ordinaci praktika<\/p>\n\n<p><b>OMEZIT péči NA PACIENTY S AKUTNÍMI POTÍŽEMI.<\/b> Pacienti, na které se vztahuje karanténa a mají zároveň klinické příznaky , dle platných postupů nutno směrovat na krajskou hygienickou stanici a linku 112.<\/p>\n\n<p><b>UKONČIT<\/b> s okamžitou platností poskytování pracovně-lékařské služby a zbytnou posudkovou činnost (řidičské a zbrojní průkazy, jiné posudky, výpisy z dokumentace atd ).Zastavit šetření pro potřebu posudkové služby ČSSZ (invalidní důchody, bezmocnosti)<\/p>\n\n<p>V okamžiku, kdy dojdou předepsané ochranné pomůcky nejsme schopni zajistit bezpečný provoz ordinace, pak dojde k převodu praxí do nouzového režimu. Ambulance budou uzavřeny, budou poskytovány pouze telefonické či elektronické konzultace, předpis e-Receptů a vystavování e-Neschopenek, bez fyzického kontaktu. Naše síly budou přesměrovány do zdravotního systému k zajištění bazální péče o akutní pacienty cestou lékařské pohotovostní služby či urgentních příjmů. Jako možné je i řešení, že péče bude zajištěna pouze v několika vybraných ordinacích v každém regionu, které budou vybaveny potřebnými ochrannými pomůckami.<\/p>\n\n<p>Vzhledem k epidemiologické situaci jsou omezeny ordinační hodiny:\n<\/p><table class=\"table table-sm table-bordered\">\n<tbody>\n<tr><td>Po:<\/td><td>7:30 – 11:30<\/td><\/tr>\n<tr><td>Út:<\/td><td>9:00 – 15:00<\/td><\/tr>\n<tr><td>St:<\/td><td>7:30 – 11:30<\/td><\/tr>\n<tr><td>Čt:<\/td><td>7:30 – 11:30<\/td><\/tr>\n<tr><td>Pá:<\/td><td>7:30 – 11:30<\/td><\/tr>\n<\/tbody><\/table>\n\n<p>OŠETŘENÍ PACIENTA V ORDINACI JEN PO TELEFONICKÉ DOMLUVĚ !!!<\/p>\n\n<p>Děkuji. MUDr. Jitka Skotáková<\/p>"
+        },
+        "footer": {
+            "content": "&copy;2020 Your website"
+        },
+        "home-1_heading": {
+            "content": "Home"
+        },
+        "home-1_subtitle": {
+            "content": ""
+        }
+    }
+}
